b116c925308895e1a824b30e739bb6b7101be0f7
Kira Mueller <50597894+Sapphiery@users.noreply.github.com>
2024-08-23 22:06:52 -0700

    add win and loss helper functions

--- code.js
+++ code.js
@@ -1,3 +1,30 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    //if it's a tie, no logic needed
+    if (humanChoice === computerChoice) {
+        console.log("Tie!");
+        console.log(`Current scores are: You: ${humanScore} Computer: ${computerScore}`);
+        return;
+    }
+
+    //check to see who won
+    if (humanChoice === "rock") {
+        
+    }
+}
+
+function roundWin() {
+    console.log("You win!");
+    console.log(`Current scores are: You: ${++humanScore} Computer: ${computerScore}`);
+}
+
+function roundLoss() {
+    console.log("You lose :(");
+    console.log(`Current scores are: You: ${humanScore} Computer: ${++computerScore}`);
+}
+
 function getComputerChoice() {
     //generate a random number between 0 and 2 and assign it to the choice variable
     let choice = Math.floor(Math.random() * 3);
@@ -18,13 +45,15 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
+    //ask the user to choose their play
     let choice = prompt("Pick rock, paper, or scissors").toLowerCase();
 
-    //keep asking for a choice untill we get a valid answer
+    //if the choice is invalid keep asking for a choice untill we get a valid answer then return it
     //valid answers are: "rock", "paper", or "scissors"
-
-
     while (!(choice === "rock" || choice === "paper" || choice === "scissors")) {
-        choice = prompt("Invalid choice. Please choose 'rock', 'paper', or 'scissors'");
+        choice = prompt("Invalid choice. Please choose 'rock', 'paper', or 'scissors'").toLowerCase();
     }
+
+    return choice;
 }
+
