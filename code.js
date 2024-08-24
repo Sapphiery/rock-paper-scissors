a9ad639ff03e454194580bba9c2a19edff3ffd08
Kira Mueller <50597894+Sapphiery@users.noreply.github.com>
2024-08-23 21:44:01 -0700

    add methods to capture human and computer choices

--- code.js
+++ code.js
@@ -1,1 +1,30 @@
-console.log("hey there");
+function getComputerChoice() {
+    //generate a random number between 0 and 2 and assign it to the choice variable
+    let choice = Math.floor(Math.random() * 3);
+
+    //if 0, choice is rock. 1 is paper. 2 is scissors
+
+    switch (choice) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+        default:
+            console.log("oops we messed up making a choice");
+            return null;
+    }
+}
+
+function getHumanChoice() {
+    let choice = prompt("Pick rock, paper, or scissors").toLowerCase();
+
+    //keep asking for a choice untill we get a valid answer
+    //valid answers are: "rock", "paper", or "scissors"
+
+
+    while (!(choice === "rock" || choice === "paper" || choice === "scissors")) {
+        choice = prompt("Invalid choice. Please choose 'rock', 'paper', or 'scissors'");
+    }
+}
